8f93f60957cc066d35f55b6b276dce58ec9eceff
7fa40e <118368827+7fa40e@users.noreply.github.com>
2025-09-21 03:14:02 +0900

    Hello worldを追加

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,7 @@ fn main() {
     class1.highest_score().unwrap().display();
     class1.lowest_score().unwrap().display();
     print!("student count: {}\n", class1.student_cont());
+
+    // ここから編集しました
+    print!("Hello World")
 }
