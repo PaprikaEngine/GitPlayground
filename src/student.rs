dc412f3c3f9ffa69c5890679cf0b7a4a775e584f
Paprika. <applezombies1125@gmail.com>
2025-09-21 02:02:31 +0900

    fix:delete Social

--- src/student.rs
+++ src/student.rs
@@ -1,29 +1,28 @@
 #[derive(Debug, Clone)]
 pub struct Student {
     name: String,
-    id: u16,
+    number: u16,
     japanese: u16,
     math: u16,
     english: u16,
     science: u16,
-    social: u16,
 }
 
 impl Student {
-    pub fn new(name: String, id: u16, japanese: u16, math: u16, english: u16, sience: u16, social: u16) ->Self {
+    pub fn new(name: String, id: u16, japanese: u16, math: u16, english: u16, sience: u16) ->Self {
         Student {
             name,
-            id,
+            number: id,
             japanese: japanese,
             math,
             english,
             science: sience,
-            social,
+
         }
     }
 
     fn total_score(&self) -> u16 {
-        self.japanese + self.math + self.english + self.science + self.social
+        self.japanese + self.math + self.english + self.science 
     }
 
     fn average_score(&self) -> f32 {
@@ -32,12 +31,11 @@ impl Student {
 
     pub fn display(&self) {
         println!("Name: {}", self.name);
-        println!("ID: {}", self.id);
+        println!("ID: {}", self.number);
         println!("Japanese: {}", self.japanese);
         println!("Math: {}", self.math);
         println!("English: {}", self.english);
         println!("Science: {}", self.science);
-        println!("Social: {}", self.social);
         println!("Total Score: {}", self.total_score());
         println!("Average Score: {}", self.average_score());
     }
